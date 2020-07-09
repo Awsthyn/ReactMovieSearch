899a324fcd3231a7e046d25d9de9e4f3a57e839a
Agustin Wagner <aguswagner008@gmail.com>
2020-07-09 14:11:16 -0300

    Updates

---
+++ precache-manifest.7ed2a6383a6e6bd3d80a22e689d33c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b176d88c4a8f036750be2dc74e4cec12",
+    "url": "/ReactMovieSearch/index.html"
+  },
+  {
+    "revision": "6968d87dbc9b704f7063",
+    "url": "/ReactMovieSearch/static/css/main.bbe02985.chunk.css"
+  },
+  {
+    "revision": "86bac7c5f2262de045e1",
+    "url": "/ReactMovieSearch/static/js/2.12346115.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMovieSearch/static/js/2.12346115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6968d87dbc9b704f7063",
+    "url": "/ReactMovieSearch/static/js/main.681114b4.chunk.js"
+  },
+  {
+    "revision": "58a81c72e608a1d7b5bc",
+    "url": "/ReactMovieSearch/static/js/runtime-main.32bec4e1.js"
+  }
+]);
